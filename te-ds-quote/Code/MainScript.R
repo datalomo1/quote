0b1c1bcb8ab53d6c43728b735f2ad77a44f2d3af
Datalomo1 <58761744+datalomo1@users.noreply.github.com>
2021-02-19 16:24:31 -0600

    Delete te-ds-quote directory

--- te-ds-quote/Code/MainScript.R
+++
@@ -1,6 +0,0 @@
-## Main Script to call Customer Quote Analysis 
-print('Setting Working Directory')
-setwd('/export/ws/prod_projects/customer_quotes/scripts/')
-print('Invoking Quote_OpportunityTable_Script.R')
-source('Quote_OpportunityTable_Script.R')
-print('End...')
